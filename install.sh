331d4437e4daff613a9738e601a8eb20ff8360c9
Daniel Felipe <daniell.felipe.2017@gmail.com>
2023-06-01 15:56:08 -0300

    feat: adding amass installation

--- install.sh
+++ install.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
-red=`tput setaf 1`
-green=`tput setaf 2`
-yellow=`tput setaf 3`
-reset=`tput sgr0`
+# colors
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+yellow=$(tput setaf 3)
+reset=$(tput sgr0)
 
-APP_PATH=~/.local/share/applications
+# path
+APP_PATH="$PATH/.local/share/applications"
 [ ! -d "$APP_PATH" ] && mkdir "$APP_PATH"
 
-echo "${green}[+] Updaing System${reset}"
-sudo pacman-mirrors -f && sudo pacman -Syyu --noconfirm
+# updating
+echo "[${green}+${reset}] Updating System"
+sudo pacman-mirrors -f
+sudo pacman -Syyu --noconfirm
 
-echo "${green}[+] Installing Packages${reset}"
+# tools
+echo "[${green}+${reset}] Installing Packages"
 [ ! -f arch.txt ] && wget https://raw.githubusercontent.com/daniel-felipe/my-dotfiles/main/packages/arch.txt -q
-sudo pacman -S - < arch.txt --needed
+sudo pacman -S $(cat arch.txt) --needed
 rm arch.txt
 
 code --version &> /dev/null
 if [ $? -ne 0 ]; then
-    echo "${green}[+] Installing Visual Studio Code${reset}"
+    echo "[${green}+${reset}] Installing Visual Studio Code"
     wget https://az764295.vo.msecnd.net/stable/252e5463d60e63238250799aef7375787f68b4ee/code-stable-x64-1683145858.tar.gz -O vscode.tar.gz -q
     wget https://gist.githubusercontent.com/infosec-intern/542d39e16f46ff472803a42bc50f3b4f/raw/a8c897aaede8eea51b8447f119cef07e41b576e2/vscode.desktop -q
     sudo tar -C /usr/local -xzf vscode.tar.gz 
@@ -28,48 +33,51 @@ fi
 
 subfinder --version &> /dev/null
 if [ $? -ne 0 ]; then
-    echo -e "${green}[+] Installing SUBFINDER \033${reset}"
+    echo "[${green}+${reset}] Installing Subfinder"
     go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
-    subfinder --version &> /dev/null
 fi
 
 httpx --version &> /dev/null
 if [ $? -ne 0 ]; then
-    echo -e "${green}[+] Installing HTTPX \033${reset}"
+    echo "[${green}+${reset}] Installing Httpx"
     go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
-    httpx --version &> /dev/null
 fi
 
 naabu --version &> /dev/null
 if [ $? -ne 0 ]; then
-    echo -e "${green}[+] Installing NAABU \033${reset}"
+    echo "[${green}+${reset}] Installing Naabu"
     go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
-    naabu --version &> /dev/null
 fi
 
 notify --version &> /dev/null
 if [ $? -ne 0 ]; then
-    echo -e "${green}[+] Installing NOTIFY \033${reset}"
+    echo "[${green}+${reset}] Installing Notify"
     go install -v github.com/projectdiscovery/notify/cmd/notify@latest
-    notify --version &> /dev/null
 fi
 
 nuclei --version &> /dev/null
 if [ $? -ne 0 ]; then
-    echo -e "${green}[+] Installing NUCLEI \033${reset}"
+    echo "[${green}+${reset}] Installing Nuclei"
     go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
-    nuclei --version &> /dev/null
 fi
 
 interactsh-client --version &> /dev/null
 if [ $? -ne 0 ]; then
-    echo -e "${green}[+] Installing INTERACT.SH \033${reset}"
+    echo "[${green}+${reset}] Installing Interactsh"
     go install -v github.com/projectdiscovery/interactsh/cmd/interactsh-client@latest
-    nuclei --version &> /dev/null
 fi
 
-echo -e "${green}[+] Installing SPOTIFY-DL"
-pip install spofify_dl
+amass --version
+if [ $? -ne 0 ]; then
+    echo "[${green}+${reset}] Installing Amass"
+    go install -v github.com/owasp-amass/amass/v3/...@master
+fi
+
+spotify_dl --version &> /dev/null
+if [ $? -ne 0 ]; then
+    echo "[${green}+${reset}] Installing SpotifyDl"
+    pip install spotify_dl
+fi
 
 starship --version &> /dev/null
 if [ $? -ne 0 ]; then
